72cef8dfa366c2f215e0f2242b9e27c6bf693c8a
lechibang1995 <130026844+lechibang1995@users.noreply.github.com>
2023-04-25 14:40:55 +0700

    Initial version of the file.
    Add note-taking app with live character counter
    
    This commit adds a simple note-taking app built using Python and PyQt5. The app allows users to create, edit, and save notes, as well as track the number of characters in their notes in real-time. The app also has a "File" menu with options to open, save, and close notes, and prompts users to save changes before closing the app.
    
    Note: This project was created entirely through ChatGPT and has no human involvement other than prompting.

---
+++ PyNoteGUI.py
@@ -0,0 +1,95 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit, QAction, QFileDialog, QMessageBox, QLabel
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # Set window title
+        self.setWindowTitle("Note Taking App")
+
+        # Create text edit widget
+        self.text_edit = QTextEdit(self)
+        self.setCentralWidget(self.text_edit)
+
+        # Create character count label
+        self.character_count_label = QLabel("Characters: 0", self)
+        self.statusBar().addWidget(self.character_count_label)
+
+        # Connect text changed signal to update_character_count slot
+        self.text_edit.textChanged.connect(self.update_character_count)
+
+        # Create file menu
+        file_menu = self.menuBar().addMenu("&File")
+
+        # Create open action
+        open_action = QAction("&Open", self)
+        open_action.setShortcut("Ctrl+O")
+        open_action.triggered.connect(self.open_file)
+        file_menu.addAction(open_action)
+
+        # Create save action
+        save_action = QAction("&Save", self)
+        save_action.setShortcut("Ctrl+S")
+        save_action.triggered.connect(self.save_file)
+        file_menu.addAction(save_action)
+
+        # Create close action
+        close_action = QAction("&Close", self)
+        close_action.setShortcut("Ctrl+W")
+        close_action.triggered.connect(self.close_file)
+        file_menu.addAction(close_action)
+
+    def open_file(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Text Files (*.txt)")
+        if filename:
+            with open(filename, "r") as file:
+                self.text_edit.setText(file.read())
+
+    def save_file(self):
+        filename, _ = QFileDialog.getSaveFileName(self, "Save File", "", "Text Files (*.txt)")
+        if filename:
+            with open(filename, "w") as file:
+                file.write(self.text_edit.toPlainText())
+            QMessageBox.information(self, "File Saved", "File saved successfully.")
+
+    def close_file(self):
+        if self.text_edit.toPlainText() != "":
+            message_box = QMessageBox()
+            message_box.setText("Do you want to save your changes?")
+            message_box.setStandardButtons(QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+            message_box.setDefaultButton(QMessageBox.Save)
+            response = message_box.exec_()
+            if response == QMessageBox.Save:
+                self.save_file()
+            elif response == QMessageBox.Cancel:
+                return
+        self.text_edit.clear()
+        QApplication.quit()
+
+    def closeEvent(self, event):
+        if self.text_edit.toPlainText() != "":
+            message_box = QMessageBox()
+            message_box.setText("Do you want to save your changes?")
+            message_box.setStandardButtons(QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+            message_box.setDefaultButton(QMessageBox.Save)
+            response = message_box.exec_()
+            if response == QMessageBox.Save:
+                self.save_file()
+            elif response == QMessageBox.Cancel:
+                event.ignore()
+                return
+        event.accept()
+
+    def update_character_count(self):
+        # Get the number of characters in the text edit
+        character_count = len(self.text_edit.toPlainText())
+
+        # Update the character count label
+        self.character_count_label.setText(f"Characters: {character_count}")
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
